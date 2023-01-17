f1c1af09be465296a795d9ff6b91a9c195a0fcb9
Vitalii <rebrun508@gmail.com>
2023-01-17 18:45:08 +0200

    update Slider

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -7,7 +7,6 @@ const Slider = () => {
 
   const [currentSlide, setCuurentSlide] = useState(0);
 
-  
   const data = [
     "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
     "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
